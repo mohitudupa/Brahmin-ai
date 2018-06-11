09fc47034d8856250a70b0bc4c52e9026d50c346
mohitudupa <33372461+mohitudupa@users.noreply.github.com>
2018-06-11 14:07:56 +0530

    UI update
    Home and registration pages

--- brahmin/model/urls.py
+++ brahmin/model/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, include
 from . import views
 
 
+app_name = 'model'
+
+
 urlpatterns = [
     path('api/', include('model.api.urls')),
+    path('home/', views.home, name="home"),
+    path('register/', views.register, name="register"),
 ]
--- brahmin/model/views.py
+++ brahmin/model/views.py
@@ -3,4 +3,13 @@ from django.contrib.auth.models import User
 
 
 # Create your views here.
+def home(request, *args, **kwargs):
+    if request.method == 'GET':
 
+        return render(request, 'model/home.html', {"error": False})
+
+
+def register(request, *args, **kwargs):
+    if request.method == 'GET':
+
+        return render(request, 'model/register.html', {"error": False})
