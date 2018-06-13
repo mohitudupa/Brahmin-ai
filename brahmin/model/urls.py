31e18348c85eb4d0a0e3f4c577510a190f9cb2fd
AdityaKP1212 <33668124+AdityaKP1212@users.noreply.github.com>
2018-06-14 00:04:40 +0530

    register view 1.1

--- brahmin/model/urls.py
+++ brahmin/model/urls.py
@@ -8,9 +8,11 @@ app_name = 'model'
 urlpatterns = [
     path('api/', include('model.api.urls')),
     path('home/', views.home, name="home"),
+    #path('login/', views.login, name="login"),
+    #path('logout/', views.user_logout, name="logout"),
     path('login/', views.user_login, name="login"),
-    path('login/form', views.login_form, name="login_form"),
     path('logout/', views.user_logout, name="logout"),
+    path('login/form', views.login_form, name="login_form"),
     path('register/', views.register, name="register"),
     path('register/form', views.register_form, name="register_form"),
     path('dashboard/', views.dashboard, name="dashboard"),
--- brahmin/model/views.py
+++ brahmin/model/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from rest_framework.authtoken.models import Token
 from django.contrib import messages
 from django.contrib.messages import get_messages
 import re
@@ -31,7 +32,7 @@ def log_instance(action, instance):
     now = datetime.datetime.now()
     key = "{0:02}-{1:02}-{2:04}".format(now.day, now.month, now.year)
     if key not in x:
-        x[key] = []        
+        x[key] = []
     x[key].append(action)
     log.update_one({"instance":instance}, {"$set": {key: x[key]}})
 
@@ -42,10 +43,10 @@ def validate(data, keys, regex, types, error):
         if isinstance(data[keys[i]], types[i]):
             if not bool(re.match(regex[i], str(data[keys[i]]))):
                 error.append("Invalid value for " + keys[i])
-                raise AssertionError()
+                raise KeyError()
         else:
             error.append("Invalid type for " + keys[i])
-            raise AssertionError()
+            raise TypeError()
     return error
 
 
@@ -87,7 +88,7 @@ def login_form(request, *args, **kwargs):
             keys = ["email", "password"]
             regex = [email, password]
             types = [str, str]
-            error = validate(data, keys, regex, types, error)   
+            error = validate(data, keys, regex, types, error)
 
             username = User.objects.get(email=data["email"])
 
@@ -120,17 +121,76 @@ def user_logout(request, *args, **kwargs):
 # Registration page view
 def register(request, *args, **kwargs):
     if request.method == "GET":
-        return render(request, "model/register.html", {"error": False})
+        if request.user.is_authenticated:
+            return redirect("model:dashboard")
+        errors = []
+        storage = get_messages(request)
+        for message in storage:
+            errors.append(str(message))
+        return render(request, "model/register.html", {"errors": errors})
 
 
 # Registration form submittion view
 def register_form(request, *args, **kwargs):
     if request.method == "POST":
-        pass
+        data = request.POST
+        print(data)
+        error = []
+
+        try:
+            post_data_keys=['first_name','last_name','username','email','password','conf_password']
+            #keys = ["first_name", "last_name", "email", "password", "username"]
+            regex = [name,name,text,email,password,password]
+            types = [str, str, str, str, str, str]
+            error = validate(data, post_data_keys, regex, types, error)
+
+            # Checking if username and email has already been taken
+
+            try:
+                User.objects.get(email=data["email"])
+                messages.info(request, "email already exists")
+                return redirect("model:register")
+            except User.DoesNotExist:
+                pass
+
+            try:
+                User.objects.get(username=data["username"])
+                messages.info(request, "username already exists")
+                return redirect("model:register")
+            except User.DoesNotExist:
+                pass
+
+        except KeyError:
+            messages.info(request, "Invalid form data values")
+            return redirect("model:register")
+            #error["error"].append("The following values are required: first_name, last_name, username,"
+            #                      "password, confirm_password and email")
+        except TypeError:
+            messages.info(request, "Invalid form data type")
+            return redirect("model:register")
+
+        # No errors are found, registering user
+        user = User.objects.create_user(data['username'], data['email'], data['password'])
+        user.first_name = data['first_name']
+        user.last_name = data['last_name']
+        user.save()
+
+        # Generate token for user
+        token = Token.objects.create(user=user)
+        token.save()
+        #authenticate user
+        user = authenticate(request, username=data["username"], password=data["password"])
+
+        if user is not None:
+            login(request, user)
+            return redirect("model:dashboard")
+        else:
+            messages.info(request, "Should not happen!")
+            return redirect("model:register")
 
 
 # User dashboard view
 @login_required(login_url="model:login")
 def dashboard(request, *args, **kwargs):
-    if request.method == "GET": 
+    if request.method == "GET":
         return render(request, "model/dashboard.html", {"error": False})
